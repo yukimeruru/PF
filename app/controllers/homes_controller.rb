0e81162143c0d643bf1c5fef32741af9c607db31
yukimeruru <yuki725421@gmail.com>
2021-11-12 22:22:27 +0000

    [add]作品投稿機能

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,6 +1,7 @@
 class HomesController < ApplicationController
 
   def top
+    @items = Item.all.order("id DESC").limit(8)
   end
 
   def about
--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,9 +1,14 @@
 class ItemsController < ApplicationController
 
   def new
+    @item = Item.new
   end
 
   def create
+    @item = Item.new(item_params)
+    @item.user_id = current_user.id
+    @item.save
+    redirect_to items_path
   end
 
   def edit
@@ -13,9 +18,12 @@ class ItemsController < ApplicationController
   end
 
   def index
+    @items = Item.all
+    @items = Item.page(params[:page]).per(12)
   end
 
   def show
+    @item = Item.find(params[:id])
   end
 
   def destroy
@@ -23,8 +31,8 @@ class ItemsController < ApplicationController
 
   private
 
-  def items_params
-    params.require(:items).permit(:name, :description, :image, :item_status, :user_id)
+  def item_params
+    params.require(:item).permit(:name, :description, :image, :item_status)
   end
 
 end
